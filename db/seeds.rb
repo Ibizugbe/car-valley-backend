bd06d0ff35dd985756a99fd005e50347a924ec18
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-11-24 11:46:55 +0100

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,14 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(username: "admin", password: "password")
-User.create(username: "Rex", password: "password")
-User.create(username: "Abraham", password: "password")
-User.create(username: "Chris", password: "password")
-User.create(username: "Daniel", password: "password")
+User.create(username: "admin", password_digest: "password", first_name: "John", last_name: "doe", dob: "2000-03-23" )
+User.create(username: "Rex", password_digest: "password", first_name: "Rex", last_name: "Silver", dob: "2010-03-23")
+User.create(username: "Abraham", password_digest: "password", first_name: "Abraham", last_name: "Smith", dob: "2003-06-20")
 
-Car.create(model: "Ford", color: "Red", price: 300)
-Car.create(model: "Honda", color: "Gray", price: 100)
-Car.create(model: "Tesla", color: "Silver", price: 10000)
-Car.create(model: "Macedes", color: "Black", price: 500)
-Car.create(model: "Torrent", color: "White", price: 0)
+Car.create(model: "Ford", color: "Red", price: 300, description: "made in Japan, on the 13th of october 2022, has a maximum speed of 233km/h", release_date: "2022-03-23" )
+Car.create(model: "Honda", color: "Gray", price: 100, description: "made in China, on the 13th of october 2022, has a maximum speed of 233km/h", release_date: "2022-03-23")
+Car.create(model: "Tesla", color: "Silver", price: 10000, description: "made in USA, on the 15th of october 2022, has a maximum speed of 233km/h", release_date: "2022-03-23")
+
 
 Reservation.create(user_id: 1, car_id: 4, date: "2022-11-23", city: "New York")
 Reservation.create(user_id: 2, car_id: 3, date: "2023-12-11", city: "Detriod")
