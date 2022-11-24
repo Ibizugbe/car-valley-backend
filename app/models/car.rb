63aaef82516a10ad180a4d23a64c4bc8d5ab9973
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-11-24 12:07:46 +0100

    add validations for car and user models

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,5 +2,7 @@ class Car < ApplicationRecord
   has_many :reservations, class_name: 'reservation'
   validates :model, presence: true
   validates :color, presence: true
+  validates :description, presence: true
+  validates :release_date, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
