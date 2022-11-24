3c26d346edadf53be714d7a1b635e88d5bffaf4b
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-11-24 12:04:22 +0100

    fix linter errors

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,6 @@
 class Reservation < ApplicationRecord
-    belongs_to :car, class_name: "Car"
-    belongs_to :user, class_name: "User"
-    validates :date, presence: true
-    validates :city, presence: true
+  belongs_to :car, class_name: 'Car'
+  belongs_to :user, class_name: 'User'
+  validates :date, presence: true
+  validates :city, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    has_many :reservations, class_name: "reservation"
-    validates :username, presence: true
-    validates :first_name, presence: true
-    validates :last_name, presence: true
+  has_many :reservations, class_name: 'reservation'
+  validates :username, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
