5889f2360413f72f1359281c53a3844ffb001af3
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-11-24 11:44:55 +0100

    update migration for reservations

--- db/migrate/20221123133735_create_cars.rb
+++ db/migrate/20221123133735_create_cars.rb
@@ -4,6 +4,9 @@ class CreateCars < ActiveRecord::Migration[7.0]
       t.string :model,     null: false, unique: true, index: true
       t.string :color,     null: false
       t.integer :price,    null: false
+      t.text :description, null: false
+      t.date :release_date, null: false
+      
 
       t.timestamps
     end
