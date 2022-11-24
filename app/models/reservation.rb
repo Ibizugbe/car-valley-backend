e77c091672239fcafcc2128a932c72817dc5fe6a
ibizugbe <abrahamibizugbe@gmail.com>
2022-11-24 04:31:08 +0100

    add validations for reservations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,6 @@
 class Reservation < ApplicationRecord
     belongs_to :car, class_name: "Car"
     belongs_to :user, class_name: "User"
+    validates :date, presence :true
+    validates :city, presence :true
 end
