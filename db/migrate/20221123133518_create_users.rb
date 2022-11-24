9bcd85691aa84621817221bd2878a7833a633470
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-11-24 11:45:20 +0100

    update migration for users table

--- db/migrate/20221123133518_create_users.rb
+++ db/migrate/20221123133518_create_users.rb
@@ -3,6 +3,10 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :username,     null: false, unique: true, index: true
       t.string :password_digest
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.date :dob, null: false
+
 
       t.timestamps
     end
