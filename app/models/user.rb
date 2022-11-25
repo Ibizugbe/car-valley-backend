2688cdcffda442cc16f18f2a8882359915962e03
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-11-26 00:02:09 +0100

    add validations for password and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :reservations, class_name: 'reservation'
-  validates :username, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :dob, presence: true
