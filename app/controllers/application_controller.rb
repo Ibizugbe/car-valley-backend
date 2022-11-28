48cbe8b8022c4898b9523468c17abd8fa34192fe
Htet Naing <htetnaing0814@gmail.com>
2022-11-28 22:25:38 +0630

    fixed rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::API
     auth_header = request.headers['Authorization']
     return unless auth_header
 
-    token = auth_header.split(' ')[1]
+    token = auth_header.split[1]
     begin
       JWT.decode(token, 'secret', true, algorithm: 'HS256')
     rescue JWT::DecodeError
